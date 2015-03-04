fafb1b5eb76d48a5ef45825a120410636b01f99d
Robert Malko <robmalko@gmail.com>
2015-03-03 23:26:36 -0500

    parse json

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -6,15 +6,20 @@ defmodule Issues.GithubIssues do
     issues_url(user, project)
       |> HTTPoison.get(@user_agent)
       |> handle_response
+      |> convert_to_list_of_hashdicts
   end
 
   def handle_response({
     :ok, %HTTPoison.Response{status_code: 200, body: body}}
-  ), do: {:ok, body}
-  def handle_response(_), do: {:error}
+  ), do: {:ok, :jsx.decode(body)}
+  def handle_response(_), do: {:error, []}
 
   def issues_url(user, project) do
     "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  def convert_to_list_of_hashdicts({_, list}) do
+    list |> Enum.map(&Enum.into(&1, HashDict.new))
   end
 
 end
